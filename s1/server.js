14c0eb4b32665f116785cccc8feecc4dabdf104e
Eduard Budacu <eduard.budacu@koding.ro>
2018-12-20 13:37:06 +0200

    implemented post method

--- s1/server.js
+++ s1/server.js
@@ -3,8 +3,11 @@ const express = require("express")
 const app = express()
 app.use('/', express.static('public'))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.post('/sendmessage', (request, response) => {
-    
+    response.send(request.body)
 })
 
 app.listen(8080);
