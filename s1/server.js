6e3468cf082581c417aee5db46d654f440059735
Eduard Budacu <eduard.budacu@gmail.com>
2020-10-13 08:49:17 +0300

    Update server.js

--- s1/server.js
+++ s1/server.js
@@ -1,13 +1,9 @@
 const express = require("express")
 
 const app = express()
-app.use('/', express.static('public'))
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
 
 app.post('/sendmessage', (request, response) => {
-    response.send(request.body)
+
 })
 
 app.listen(8080);
